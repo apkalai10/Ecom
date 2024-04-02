f3e7e3d12e3d548cdbb7fc8d3d59c301870da47b
Shobin@2023 <aravinthprasath3@gmail.com>
2024-04-02 23:20:38 +0530

    driver updated

--- com.amazon/src/main/java/com/webdriver/Driver.java
+++ com.amazon/src/main/java/com/webdriver/Driver.java
@@ -1,5 +1,33 @@
 package com.webdriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
 public class Driver {
+	
+	public static WebDriver driver;
+	
+	
+	public static WebDriver startBrowser(String browserName)
+	{
+		if(browserName.equalsIgnoreCase("FIREFOX"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else if (browserName.equalsIgnoreCase("CHROME")) 
+		{
+			driver = new ChromeDriver();
+		}
+		else if (browserName.equalsIgnoreCase("EXPLORER")) 
+		{
+			driver = new InternetExplorerDriver();
+		}
+		else {
+			System.out.println("Given driver is not available in source");
+		}
+		return driver;
+	}
 
 }
