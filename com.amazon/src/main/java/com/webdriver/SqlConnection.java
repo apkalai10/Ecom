492a70da79238441b5624d58b162deacb654f50e
Shobin@2023 <aravinthprasath3@gmail.com>
2024-04-11 08:06:34 +0530

    changed in sql connection

--- com.amazon/src/main/java/com/webdriver/SqlConnection.java
+++ com.amazon/src/main/java/com/webdriver/SqlConnection.java
@@ -11,24 +11,26 @@ public class SqlConnection {
 
 	public static void main(String[] args)   {
 		try {
-			//String url = "jdbc:sqlserver://localhost:1433;authenticationScheme=NTLM;databaseName=arvind;encrypt=true;trustServerCertificate=true;domain=myDomain"
-			String url = "jdbc:sqlserver://localhost:1433;databaseName=arvind;trustServerCertificate=true";
-			Connection con = DriverManager.getConnection(url, "Tomcat", "Tomcat@2023");
-			Statement state = con.createStatement();
-			ResultSet rs = state.executeQuery("select * from student");
+			//String url = "jdbc:sqlserver://localhost:1433;authenticationScheme=NTLM;databaseName=arvind;encrypt=true;trustServerCertificate=true;domain=myDomain";
+			String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=arvind;trustServerCertificate=true";
+			String username="Tomcat";
+			String password="Tomcat@2023";
+			Connection connection = DriverManager.getConnection(connectionUrl, username, password);
+			Statement statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery("select * from student");
 	
 			while(rs.next())
 			{
 				System.out.println("name = " + rs.getString("s_name"));
 				
 			}
-			state.executeUpdate("insert into student(s_name) values('rachel')");
+			statement.executeUpdate("insert into student(s_name) values('rachel')");
 			
-			ResultSet rs1 = state.executeQuery("select s_dob from student where s_roolno=4");
+			ResultSet rs1 = statement.executeQuery("select s_dob from student where s_roolno=4");
 			rs1.next();
 			Date date = rs1.getDate("s_dob");
 			System.out.println(date);
-			con.close();
+			connection.close();
 	} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
